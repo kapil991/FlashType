58b77ea2340888a4983fc469bcbeb046d8203d7a
kapil991 <gkapil991@gmail.com>
2021-06-30 19:28:55 +0530

    first commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+    return (
+        <div className="footer-container">
+            <a
+                target="_blank"
+                className="footer-link"
+                href="https://github.com/kapil991/FlashType"
+                rel="noreferrer"
+            >
+                GitHub code link here
+            </a>
+        </div>
+    );
+};
+
+export default Footer;
